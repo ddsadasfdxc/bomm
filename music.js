2b6c770f90c9cc4b62a0f5a1f5ea2c46f5281e24
Test User <test@example.com>
2026-06-08 01:47:40 +0000

    fix: use full-size NetEase playlist player (370px)
    - Switch from mini player (90px) to full song list player (370px)
    - Add panel inner wrapper for better styling
    - Improve mobile panel sizing
    - Add loading indicator

--- music.js
+++ music.js
@@ -1,16 +1,19 @@
 (function() {
-    // Inject music widget HTML
     var widget = document.createElement('div');
     widget.className = 'music-widget';
     widget.innerHTML = 
         '<div class="music-panel" id="musicPanel">' +
-            '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="90" src="https://music.163.com/outchain/player?type=0&id=3779629&auto=0&height=90" style="width: 100%; height: 90px; display: block;"></iframe>' +
+            '<div class="music-panel-inner">' +
+                '<div class="music-loading" id="musicLoading">Loading...</div>' +
+                '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="370" src="https://music.163.com/outchain/player?type=0&id=3779629&auto=0&height=370" style="width:100%;height:370px;display:block;border-radius:12px;"></iframe>' +
+            '</div>' +
         '</div>' +
         '<button class="music-btn" id="musicBtn" title="Music Player">🎵</button>';
     document.body.appendChild(widget);
 
     var btn = document.getElementById('musicBtn');
     var panel = document.getElementById('musicPanel');
+    var loading = document.getElementById('musicLoading');
 
     btn.addEventListener('click', function(e) {
         e.stopPropagation();
