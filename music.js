118d3009267a3f58f4b9afbad308c58fd70f33ad
Test User <test@example.com>
2026-06-08 01:56:30 +0000

    feat: connect to user's NetEase playlist
    - Switch to playlist id=12675557343 (虚数解叙事喜欢的音乐)

--- music.js
+++ music.js
@@ -5,7 +5,7 @@
         '<div class="music-panel" id="musicPanel">' +
             '<div class="music-panel-inner">' +
                 '<div class="music-loading" id="musicLoading">Loading...</div>' +
-                '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="370" src="https://music.163.com/outchain/player?type=0&id=3779629&auto=0&height=370" style="width:100%;height:370px;display:block;border-radius:12px;"></iframe>' +
+                '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="370" src="https://music.163.com/outchain/player?type=0&id=12675557343&auto=0&height=370" style="width:100%;height:370px;display:block;border-radius:12px;"></iframe>' +
             '</div>' +
         '</div>' +
         '<button class="music-btn" id="musicBtn" title="Music Player">🎵</button>';
