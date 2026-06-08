5aa38c5bc186355db3f31006c11e5f84ec2371d5
Test User <test@example.com>
2026-06-08 01:58:18 +0000

    fix: remove loading indicator blocking iframe display

--- music.js
+++ music.js
@@ -4,7 +4,6 @@
     widget.innerHTML = 
         '<div class="music-panel" id="musicPanel">' +
             '<div class="music-panel-inner">' +
-                '<div class="music-loading" id="musicLoading">Loading...</div>' +
                 '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="330" height="370" src="https://music.163.com/outchain/player?type=0&id=12675557343&auto=0&height=370" style="width:100%;height:370px;display:block;border-radius:12px;"></iframe>' +
             '</div>' +
         '</div>' +
@@ -13,7 +12,6 @@
 
     var btn = document.getElementById('musicBtn');
     var panel = document.getElementById('musicPanel');
-    var loading = document.getElementById('musicLoading');
 
     btn.addEventListener('click', function(e) {
         e.stopPropagation();
